34c8db2e37ffccbb244ba042f497326f45d7cfd2
Levi Aul <levi@leviaul.com>
2016-10-31 16:52:26 -0700

    Config needs to live in the scaffold app to override that of the hosted apps

---
+++ node/apps/scaffold/config/config.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+config :elixir,
+  ansi_enabled: true
+
+config :exuvia,
+  host_key: {:dir, "/run/exuvia/host-keys"},
+  auth: Exuvia.KeyBag.Github
+
+config :logger,
+	level: :info,
+  utc_log: true,
+  handle_otp_reports: true,
+  handle_sasl_reports: true,
+	colors: [enabled: true],
+	backends: []
--- node/config/config.exs
+++ node/config/config.exs
@@ -1,18 +1,3 @@
 use Mix.Config
 
 import_config "../apps/*/config/config.exs"
-
-config :elixir,
-  ansi_enabled: true
-
-config :exuvia,
-  host_key: {:dir, "/run/exuvia/host-keys"},
-  auth: Exuvia.KeyBag.Github
-
-config :logger,
-	level: :info,
-  utc_log: true,
-  handle_otp_reports: true,
-  handle_sasl_reports: true,
-	colors: [enabled: true],
-	backends: []
